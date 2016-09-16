baeda62467d30b572a4478b68bfbf799073c660d
nandini <nandini.r.nayak@gmail.com>
2016-09-16 11:56:58 +1000

    devise,bootstrap and nav implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :tweets
   devise_for :users
+  root 'pages#feed'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
